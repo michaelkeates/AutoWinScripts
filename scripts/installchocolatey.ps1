93d733807c9c75e71b569ef42a482ed6a51332cd
michaelkeates <mail@michaelkeates.co.uk>
2023-08-04 22:42:12 +0100

    added version for packages
    can state a version for a package, if blank after the comma the latest will be download

--- scripts/installchocolatey.ps1
+++ scripts/installchocolatey.ps1
@@ -13,10 +13,15 @@ $scriptDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Path
 #specify the relative path to the packages.config file
 $packagesConfigPath = Join-Path -Path $scriptDirectory -ChildPath "..\packages.config"
 
-#function to process package installation
-function InstallPackage($packageName) {
-    #run the package installation
-    choco install -y $packageName
+#function to process package installation with version handling
+function InstallPackage($packageName, $version) {
+    #if a specific version is provided, use `choco install --version`
+    if ($version) {
+        & choco upgrade -y $packageName --version $version
+    } else {
+        #if no version is provided, use `choco upgrade` to install the latest version
+        & choco upgrade -y $packageName
+    }
 }
 
 #process package configuration
@@ -28,15 +33,19 @@ if (Test-Path $packagesConfigPath) {
     foreach ($line in $packagesConfig) {
         #skip comment lines that begin with #
         if (-not ($line -match '^\s*#')) {
-            #extract the package name from the line
-            $packageName = ($line -split "\r\n")[0].Trim()
+            #extract the package name and version from the line
+            $packageName, $version = $line -split ",", 2
+            $packageName = $packageName.Trim()
+            if ($version) {
+                $version = $version.Trim()
+            }
 
             #process package installation
-            InstallPackage -packageName $packageName
+            InstallPackage -packageName $packageName -version $version
         }
     }
 }
 else {
-    #will update this to grab latest from github repo
+    #will update this to grab latest from GitHub repo
     Write-Host "packages.config file not found in the expected location."
 }
