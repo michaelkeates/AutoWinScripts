bfc0fd011e0b1f295464ef210b134d64ac770426
Michael Keates <mail@michaelkeates.co.uk>
2023-12-02 21:00:04 +0000

    Update removeapps_new.ps1

--- scripts/removeapps/removeapps_new.ps1
+++ scripts/removeapps/removeapps_new.ps1
@@ -35,53 +35,57 @@ function RunScript($scriptUrl) {
     }
 }
 
-
 # Function to enable Remote Desktop firewall rule
 function EnableRemoteDesktopFirewallRule() {
     netsh advfirewall firewall set rule group="Remote Desktop" new enable=Yes
 }
 
-# Attempt to download the registry.config file
-Invoke-WebRequest -Uri $removeappsConfigUrl -OutFile "$scriptDirectory\removeapps.config"
+# Attempt to download the removeapps.config file
+try {
+    Invoke-WebRequest -Uri $removeappsConfigUrl -OutFile "$scriptDirectory\removeapps.config"
+    
+    if (Test-Path "$scriptDirectory\removeapps.config") {
+        Write-Host "removeapps.config file downloaded successfully."
 
-# Process app removal configuration
-if (Test-Path "$scriptDirectory\removeapps.config") {
-    # Read the contents of the removeapps.config file
-    $removeappsConfig = Get-Content "$scriptDirectory\removeapps.config"
+        # Read the contents of the removeapps.config file
+        $removeappsConfig = Get-Content "$scriptDirectory\removeapps.config"
 
-    # Flag to track if Microsoft.RemoteDesktop is enabled in the config
-    $remoteDesktopEnabled = $false
+        # Flag to track if Microsoft.RemoteDesktop is enabled in the config
+        $remoteDesktopEnabled = $false
 
-    # For each line in the removeapps.config file
-    foreach ($line in $removeAppsConfig) {
-        # Skip comment lines that begin with #
-        if (-not ($line -match '^\s*#')) {
-            # Extract the app name from the line
-            $appName = $line.Trim()
+        # For each line in the removeapps.config file
+        foreach ($line in $removeAppsConfig) {
+            # Skip comment lines that begin with #
+            if (-not ($line -match '^\s*#')) {
+                # Extract the app name from the line
+                $appName = $line.Trim()
 
-            # Check if the app name is Microsoft.RemoteDesktop
-            if ($appName -eq "Microsoft.RemoteDesktop") {
-                # Set the flag to true if not commented out
-                $remoteDesktopEnabled = $true
-            }
-            else {
-                # Process app removal
-                RemoveApp -appName $appName
+                # Check if the app name is Microsoft.RemoteDesktop
+                if ($appName -eq "Microsoft.RemoteDesktop") {
+                    # Set the flag to true if not commented out
+                    $remoteDesktopEnabled = $true
+                }
+                else {
+                    # Process app removal
+                    RemoveApp -appName $appName
 
-                # Check if the app name is Microsoft.Edge exists and run another script
-                if ($appName -eq "Microsoft.Edge") {
-                    $edgeRemovalScriptUrl = "https://raw.githubusercontent.com/michaelkeates/AutoWinScripts/main/scripts/removeapps/removeedge.ps1"
-                    RunScript -scriptUrl $edgeRemovalScriptUrl
+                    # Check if the app name is Microsoft.Edge exists and run another script
+                    if ($appName -eq "Microsoft.Edge") {
+                        $edgeRemovalScriptUrl = "https://raw.githubusercontent.com/michaelkeates/AutoWinScripts/main/scripts/removeapps/removeedge.ps1"
+                        RunScript -scriptUrl $edgeRemovalScriptUrl
+                    }
                 }
             }
         }
-    }
 
-    # Check if Microsoft.RemoteDesktop is not enabled and enable the firewall rule
-    if (-not $remoteDesktopEnabled) {
-        EnableRemoteDesktopFirewallRule
+        # Check if Microsoft.RemoteDesktop is not enabled and enable the firewall rule
+        if (-not $remoteDesktopEnabled) {
+            EnableRemoteDesktopFirewallRule
+        }
+    } else {
+        Write-Host "removeapps.config file not found in the expected location."
     }
 }
-else {
-    Write-Host "removeapps.config file not found in the expected location."
+catch {
+    Write-Host "Failed to download removeapps.config file from $removeappsConfigUrl. Error: $_"
 }
